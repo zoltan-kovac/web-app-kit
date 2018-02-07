21c7c5e842b5e673725d67201c0fdf2ec861384c
Zoltán Kováč <zoltan.kovac@actum.cz>
2018-02-07 22:03:03 +0100

    webpack config init; core app add; initial folder structure

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        app: './src/app.js'
+    },
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
